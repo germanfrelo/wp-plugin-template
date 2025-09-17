22962b9d545d69fe43b086f028d30f1eb453b902
Germán Freixinós <14282156+germanfrelo@users.noreply.github.com>
2025-09-17 23:12:58 +0200

    refactor: Remove unnecessary files from template

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-export default {
-	plugins: {
-		"postcss-import-ext-glob": {}, // This plugin must be placed before the "postcss-import" plugin
-		"postcss-import": {},
-		"postcss-nesting": {},
-		"postcss-custom-media": {},
-		"postcss-discard-comments": {},
-	},
-};
